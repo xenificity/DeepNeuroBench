674114a1b4e02a64a184a85b14be17952fbee45c
xenificity <ajay.ducs@gmail.com>
2026-06-02 18:41:02 +0000

    updated

--- results/preprocessing.sh
+++
@@ -1,4 +0,0 @@
-# Shell commands to convert chess dataset into deepprep required format
-for i in sub-*;do echo $i; cd ${i}/ses-01/anat;echo ${i}_T1w.json;cd ~/chess_dataset/bids;done
-for i in sub-*;do echo $i; cd ${i}/ses-01/func;mv ${i}_task-rest_bold.json ${i}_ses-01_task-rest_run-01_bold.json; mv ${i}_task-rest_bold.nii.gz ${i}_ses-01_task-rest_run-01_bold.nii.gz ;cd ~/chess_dataset/bids;done
-for i in sub*;do echo $i;cd ${i}/ses-01;rm -r -f dwi;cd ../..;done
--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-"""Test configuration: make ``src/`` importable as a top-level module."""
-
-import sys
-from pathlib import Path
-
-ROOT = Path(__file__).resolve().parent.parent
-SRC = ROOT / "src"
-if str(SRC) not in sys.path:
-    sys.path.insert(0, str(SRC))
